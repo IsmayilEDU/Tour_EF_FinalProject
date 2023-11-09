0997e1b02522b6cf9f4bfd18ddff1af6148b2264
ismayilkerimzade <ismayil.programmer@gmail.com>
2023-11-09 06:38:07 +0400

    Reload project wpf

---
+++ TourFinalProject_EF/WpfApp/ViewModels/AdminViewModels/AdminViewModel.cs
@@ -0,0 +1,276 @@
+﻿using Database.Contexts;
+using GalaSoft.MvvmLight.Command;
+using Models.DTO;
+using Models.Entities.DerivedEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WpfApp.Views.AdminViews;
+using WpfApp.Views.StartViews;
+
+namespace WpfApp.ViewModels.AdminViewModels
+{
+    class AdminViewModel
+    {
+
+        #region Fields
+        private AdminView _thisView;
+        public List<TourDTO> TourDTOs { get; set; }
+        public List<TouristDTO> TouristDTOs { get; set; }
+        public List<TourleaderDTO> TourleaderDTOs { get; set; }
+        public List<CarDTO> CarDTOs { get; set; }
+        public List<DriverDTO> DriverDTOs { get; set; }
+        public List<LocationDTO> LocationDTOs { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public RelayCommand ExitCommand { get; set; }
+
+        #endregion
+
+        #region Command functions
+        private void exit()
+        {
+            new LoginView().Show();
+            _thisView.Close();
+        }
+        #endregion
+
+        #region DB_Functions
+        private List<TourDTO> TourDTOFromDB(ref TourDbContext tourDb)
+        {
+
+            List<TourDTO> tourDTOs = new();
+            List<Tour> tours = tourDb.Tours.ToList();
+            foreach (var tour in tours)
+            {
+
+                #region Find tourleader name with inner join
+
+                var tourleaderName = (from Tour in tourDb.Tours
+                                      join Tourleader in tourDb.Tourleaders on Tour.TourleaderId equals Tourleader.Id
+                                      where Tour.Id == tour.Id
+                                      select Tourleader.Name).FirstOrDefault();
+
+                #endregion
+
+                #region Add number of car which relation tour
+
+                var numbersOfCars = (from Tour in tourDb.Tours
+                                     join carTour in tourDb.CarTours on Tour.Id equals carTour.TourId
+                                     join car in tourDb.Cars on carTour.CarId equals car.Id
+                                     where Tour.Id == tour.Id
+                                     select car.CarNumber).ToList();
+
+                #endregion
+
+                #region Add name of location which relation tour
+
+                var namesOfLocations = (from Tour in tourDb.Tours
+                                        join tourLocation in tourDb.TourLocations on Tour.Id equals tourLocation.TourId
+                                        join location in tourDb.Locations on tourLocation.LocationId equals location.Id
+                                        where Tour.Id == tour.Id
+                                        select location.Name).ToList();
+
+                #endregion
+
+                #region IDs of tckets which relation with tour
+
+                var iDsOfTickets = (from Tour in tourDb.Tours
+                                    join ticket in tourDb.Tickets on Tour.Id equals ticket.TourId
+                                    where Tour.Id == tour.Id
+                                    select ticket.Id).ToList();
+                #endregion
+
+                #region Create tourDTO
+
+                var tourDTO = new TourDTO
+                {
+                    Id = tour.Id,
+                    StartTime = DateOnly.FromDateTime(tour.StartTime),
+                    FinishTime = DateOnly.FromDateTime(tour.FinishTime),
+                    TourleaderName = tourleaderName!,
+                    IsActive = tour.IsActive,
+                    NumbersOfCars = numbersOfCars,
+                    NamesOfLocations = namesOfLocations,
+                    IDsOfTickets = iDsOfTickets
+                };
+
+                #endregion
+
+                tourDTOs.Add(tourDTO);
+            }
+            return tourDTOs;
+        }
+
+        private List<TouristDTO> TouristDTOFromDB(ref TourDbContext tourDb)
+        {
+
+            List<TouristDTO> touristDTOs = new();
+            List<Tourist> tourists = tourDb.Tourists.ToList();
+            foreach (var tourist in tourists)
+            {
+
+                #region Find balance of bankcard whiceh relation with tourist
+
+                var balanceOfBankcard = (from Tourist in tourDb.Tourists
+                                         join Bankcard in tourDb.BankCards on Tourist.Id equals Bankcard.TouristId
+                                         where Tourist.Id == tourist.Id
+                                         select Bankcard.Balance).FirstOrDefault();
+
+                #endregion
+
+                #region IDs of tickets which relation with tourist
+
+                var iDsOfTickets = (from Tourist in tourDb.Tourists
+                                    join ticket in tourDb.Tickets on Tourist.Id equals ticket.TouristId
+                                    where Tourist.Id == tourist.Id
+                                    select ticket.Id).ToList();
+                #endregion
+
+                #region Create tourDTO
+
+                var touristDTO = new TouristDTO
+                {
+                    Id = tourist.Id,
+                    Username = tourist.Username,
+                    Password = tourist.Password,
+                    Name = tourist.Name,
+                    Surname = tourist.Surname,
+                    Phone = tourist.Phone,
+                    BalanceOfBankcard = balanceOfBankcard,
+                    IDsOfTickets = iDsOfTickets,
+                };
+
+                #endregion
+
+                touristDTOs.Add(touristDTO);
+            }
+            return touristDTOs;
+        }
+
+        private List<TourleaderDTO> TourleaderDTOFromDB(ref TourDbContext tourDb)
+        {
+            List<TourleaderDTO> tourleaderDTOs = new();
+            List<Tourleader> tourleaders = tourDb.Tourleaders.ToList();
+
+            foreach (var tourleader in tourleaders)
+            {
+                var iDsOfTours = (from Tourleader in tourDb.Tourleaders
+                                  join Tour in tourDb.Tours on tourleader.Id equals Tour.TourleaderId
+                                  where Tourleader.Id == tourleader.Id
+                                  select Tourleader.Id).ToList();
+
+                var tourleaderDTO = new TourleaderDTO()
+                {
+                    Id = tourleader.Id,
+                    Name = tourleader.Name,
+                    Surname = tourleader.Surname,
+                    Phone = tourleader.Phone,
+                    IDsOfTours = iDsOfTours
+                };
+            }
+
+            return tourleaderDTOs;
+        }
+
+        private List<CarDTO> CarDTOFromDB(ref TourDbContext tourDb)
+        {
+            List<CarDTO> carDTOs = new();
+            List<Car> cars = tourDb.Cars.ToList();
+
+            foreach (var car in cars)
+            {
+                var driverName = (from Car in tourDb.Cars
+                                  join Driver in tourDb.Drivers on Car.Id equals Driver.CarId
+                                  where car.Id == Car.Id
+                                  select Driver.Name).FirstOrDefault();
+
+                var carDTO = new CarDTO()
+                {
+                    Id = car.Id,
+                    Brand = car.Brand,
+                    Model = car.Model,
+                    Year = car.Year,
+                    SeatCount = car.SeatCount,
+                    CarNumber = car.CarNumber,
+                    DriverName = driverName!
+                };
+            }
+            return carDTOs;
+        }
+
+        private List<DriverDTO> DriverDTOFromDB(ref TourDbContext tourDb)
+        {
+            List<DriverDTO> driverDTOs = new();
+            List<Driver> drivers = tourDb.Drivers.ToList();
+
+            foreach (var driver in drivers)
+            {
+                var carNumber = (from Car in tourDb.Cars
+                                 join Driver in tourDb.Drivers on Car.Id equals Driver.CarId
+                                 where driver.Id == Driver.Id
+                                 select Car.CarNumber).FirstOrDefault();
+
+                var driverDTO = new DriverDTO()
+                {
+                    Id = driver.Id,
+                    Name = driver.Name,
+                    Surname = driver.Surname,
+                    Phone = driver.Phone,
+                    driverLicenseCategory = driver.driverLicenseCategory,
+                    CarNumber = carNumber!,
+
+                };
+            }
+            return driverDTOs;
+        }
+
+        private List<LocationDTO> LocationDTOFromDB(ref TourDbContext tourDb)
+        {
+            List<LocationDTO> locationDTOs = new();
+            List<Location> locations = tourDb.Locations.ToList();
+
+            foreach (var location in locations)
+            {
+                var iDsOfTours = (from Tour in tourDb.Tours
+                                  join tourLocation in tourDb.TourLocations on Tour.Id equals tourLocation.TourId
+                                  join Location in tourDb.Locations on tourLocation.LocationId equals Location.Id
+                                  where location.Id == Location.Id
+                                  select Tour.Id).ToList();
+
+                var locationDTO = new LocationDTO()
+                {
+                    Id = location.Id,
+                    Name = location.Name,
+                    Longitude = location.Longitude,
+                    Latitude = location.Latitude,
+                    IDsOfTours = iDsOfTours
+                };
+                locationDTOs.Add(locationDTO);
+
+            }
+            return locationDTOs;
+        }
+
+
+        #endregion
+
+        #region Constructor
+        public AdminViewModel(AdminView thisView,ref TourDbContext tourDb)
+        {
+            ExitCommand = new RelayCommand(exit);
+            _thisView= thisView;
+            TourDTOs = TourDTOFromDB(ref tourDb);
+            TouristDTOs = TouristDTOFromDB(ref tourDb);
+            TourleaderDTOs = TourleaderDTOFromDB(ref tourDb);
+            CarDTOs = CarDTOFromDB(ref tourDb);
+            DriverDTOs = DriverDTOFromDB(ref tourDb);
+            LocationDTOs = LocationDTOFromDB(ref tourDb);
+        }
+        #endregion
+
+    }
+}
---
+++ TourFinalProject_EF/WpfApp/ViewModels/StartViewModels/LoginViewModel.cs
@@ -0,0 +1,110 @@
+﻿using Database.Contexts;
+using Models.Entities.DerivedEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using GalaSoft.MvvmLight.Command;
+using WpfApp.Views.StartViews;
+using WpfApp.Views.AdminViews;
+
+namespace WpfApp.ViewModels.StartViewModels
+{
+    class LoginViewModel
+    {
+
+        #region Fields
+        private LoginView _thisView;
+        private readonly TextBox _textbox_Username;
+        private readonly PasswordBox _passwordBox_Password;
+        private readonly RadioButton _radiobutton_Admin;
+        private readonly RadioButton _radiobutton_Tourist;
+        public TourDbContext DbContext { get; set; }
+        #endregion
+
+        #region Commands
+        public RelayCommand SignIn_Command { get; set; }
+        public RelayCommand Register_Command { get; set; }
+        #endregion
+
+        #region Command functions
+        private void SignIn()
+        {
+            try
+            {
+                //  If user want to sign in like admin
+
+                if (_radiobutton_Admin.IsChecked == true)
+                {
+                    if (CheckAdmin(in _textbox_Username, in _passwordBox_Password))
+                    {
+                        new AdminView().Show();
+                        _thisView.Close();
+                    }
+                    else
+                    {
+                        throw new Exception("Username or password of admin incorrect!");
+                    }
+                }
+
+                //  If user want to sign in like tourist
+                else if (_radiobutton_Tourist.IsChecked == true)
+                {
+
+                    Tourist SelectedTourist;
+                    List<Tourist> tourists = DbContext.Tourists.ToList();
+                    foreach (var tourist in tourists)
+                    {
+
+                    }
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Register()
+        {
+            new RegisterView().Show();
+            _thisView.Close();
+        }
+        #endregion
+
+        #region Other functions
+        private bool CheckAdmin(in TextBox textbox_Username, in PasswordBox _passwordBox_Password)
+        {
+            return textbox_Username.Text == MyConfigurations.MyConfigurations.UsernameOfAdmin
+                                            &&
+                                            _passwordBox_Password.Password == MyConfigurations.MyConfigurations.PasswordOfAdmin;
+        }
+
+        #endregion
+
+        #region Constructor
+        public LoginViewModel
+                (
+                    LoginView thisView,
+                    ref TextBox textbox_Username, ref PasswordBox passwordBox_Password,
+                    ref RadioButton radiobutton_Admin, ref RadioButton radiobutton_Tourist
+                )
+        {
+            SignIn_Command = new RelayCommand(SignIn);
+            Register_Command = new RelayCommand(Register);
+            _thisView = thisView;
+            _textbox_Username = textbox_Username;
+            _passwordBox_Password = passwordBox_Password;
+            _radiobutton_Admin = radiobutton_Admin;
+            _radiobutton_Tourist = radiobutton_Tourist;
+            DbContext = new();
+        }
+        #endregion
+
+    }
+}
